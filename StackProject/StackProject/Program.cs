0df4e9f0d689151b8b0f649c99887f6df9ddccb9
ToncuElenaSonia <sonya_helena@yahoo.com>
2019-07-04 23:15:51 +0300

    Stack problem solved

---
+++ StackProject/StackProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackProject
+{
+    class Program
+    {
+        //Ajungeti la ultimul element fara sa modificati elementele din stack.
+        static void Main(string[] args)
+        {
+            ReturnLastElementFromStack();       
+        }
+        public static void ReturnLastElementFromStack()
+        {
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("Monday");
+            myStack.Push("Tuesday");
+            myStack.Push("Wednesday");
+            myStack.Push("Thursday");
+            myStack.Push("Friday");
+            myStack.Push("Saturday");
+            myStack.Push("Sunday");
+            string[] myArray = myStack.ToArray();
+            foreach (string item in myArray)
+            {
+                Console.WriteLine(item);
+                if (item == myArray[myArray.Length - 1])
+                {
+                    Console.WriteLine($"The last element from the stack is {item}");
+                }
+            }
+        }
+    }
+}
